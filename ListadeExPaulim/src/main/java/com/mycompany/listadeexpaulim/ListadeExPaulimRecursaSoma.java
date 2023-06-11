bc10d9a149cf4d0c42ed95c94116089a50f64706
Guinho2307 <108555041+Guinho2307@users.noreply.github.com>
2023-06-11 19:25:51 -0300

    Add files via upload

--- ListadeExPaulim/src/main/java/com/mycompany/listadeexpaulim/ListadeExPaulimRecursaSoma.java
+++ ListadeExPaulim/src/main/java/com/mycompany/listadeexpaulim/ListadeExPaulimRecursaSoma.java
@@ -18,7 +18,8 @@ public class ListadeExPaulimRecursaSoma {
       
               option = Integer.parseInt(JOptionPane.showInputDialog("Escolha um dos exercicios \n"
                       + "1 para Soma recursiva de 0 a 50\n"
-                      + "2 Para Recursiva Fatorial\n "));
+                      + "2 Para Recursiva Fatorial\n "
+                      + "3 Para Fibonacci"));
         if(option.equals(1)){
             int resultado = somar(0, 50);
         System.out.println("Resultado: " + resultado);  
@@ -31,8 +32,31 @@ public class ListadeExPaulimRecursaSoma {
         int resultado = calcularFatorial(numero);
 
         System.out.println("O fatorial de " + numero + " é: " + resultado);
+        
+    }else if (option.equals(3)){
+         
+        
+        Integer numero = Integer.parseInt(JOptionPane.showInputDialog("Informe um número inteiro não negativo: "));
+    
+
+        System.out.println("Sequência Fibonacci de " + numero + ":");
+        for (int i = 0; i <= numero; i++) {
+            System.out.print(calcularFibonacci(i) + " ");
+        }
     }
- }
+}
+
+    public static int calcularFibonacci(int numero) {
+        if (numero == 0) {
+            return 0;
+        } else if (numero == 1) {
+            return 1;
+        } else {
+            return calcularFibonacci(numero - 1) + calcularFibonacci(numero - 2);
+        }
+    }
+ 
+
 
     public static int calcularFatorial(int numero) {
         if (numero == 0 || numero == 1) {
